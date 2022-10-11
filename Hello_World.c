4fef6d924042426357d7febaff60f90e6ab46f59
FERNANDEZ, J <115331757+justsammy123@users.noreply.github.com>
2022-10-11 17:32:29 +0800

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+
+           //declaration of variables
+           int num1 = 2022;
+           char letter = 'a';
+           float num2 = 3.1416);
+
+           //printing the variables
+           printf("value of num1 is %d\n", num1);
+           printf("value of letter is %c\n", letter);
+           printf("value of num2 is %f", num2);
+
+    return 0;
+}
